d0dbc93acd3a40f89180a6428f951dcc858abea7
tearne <tearne@gmail.com>
2016-11-16 20:37:59 +0000

    ImageData test

--- src/main/scala/proto/Spira.scala
+++ src/main/scala/proto/Spira.scala
@@ -41,7 +41,7 @@ object Spira {
 
 import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom
-import org.scalajs.dom.html
+import org.scalajs.dom.{ImageData, html}
 
 @JSExport
 object Main {
@@ -52,28 +52,36 @@ object Main {
 
     var count = 0
     var p = Point(0, 0)
-    val corners = Seq(Point(800, 800), Point(0, 800), Point(400, 0))
+    val corners = Seq(Point(300, 300), Point(0, 300), Point(150, 0))
 
     def clear() = {
       ctx.fillStyle = "black"
-      ctx.fillRect(0, 0, 800, 800)
+      ctx.fillRect(0, 0, 100, 100)
     }
 
     def run() = {
-
-      if (count % 3000 == 0) clear()
-      count += 1
-
       Spira.points.foreach { pt =>
         println(pt)
         val p = pt * 10+ Point(100,100)
         ctx.fillStyle = s"rgb(255, 10, 10)"
         ctx.fillRect(p.x, p.y, 1, 1)
       }
-
     }
 
     println("RUN")
     run()
+    val imgDat = ctx.getImageData(0 ,0 , 300, 300)
+    (0 until (300 * 300 * 4)).foreach{i =>
+      i % 4 match{
+        case 0 => imgDat.data(i) = 155
+        case 1 => imgDat.data(i) = 70
+        case 2 => imgDat.data(i) = 130
+        case 3 => imgDat.data(i) = 255
+      }
+      imgDat.data(i)
+    }
+    println("+++++++++++")
+    println(imgDat)
+    ctx.putImageData(imgDat,0.0, 0.0)
   }
 }
